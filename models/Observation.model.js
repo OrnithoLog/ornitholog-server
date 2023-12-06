fa3aa2354ebf53db79ba244b5d4e9411c90c04c6
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-06 11:40:41 +0100

    Creating Observation Model

---
+++ models/Observation.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const observationSchema = new Schema({
+  name: { type: String, required },
+  date: { type: Date, required },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  habitat: {
+    type: String,
+    required,
+    enum: [
+      "forest",
+      "grassland",
+      "wetland",
+      "coast",
+      "urban",
+      "mountain",
+      "river",
+    ],
+  },
+  vegetation: { type: String },
+  age: { enum: ["juvenil", "adult"] },
+  photo: {
+    type: String,
+    default:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png",
+  },
+  sound: { type: String },
+  temperature: { type: Number },
+  birdId: { type: Schema.Types.ObjectId, ref: "Bird" },
+});
+
+const Observation = model("Observation", observationSchema);
+
+module.exports = Observation;

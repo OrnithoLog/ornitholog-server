63136a8fcd81a5a66a7df1976f623850c4f99294
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-06 14:33:26 +0100

    creating observation routes

--- models/Observation.model.js
+++ models/Observation.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const observationSchema = new Schema({
   name: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, default: Date.now },
   location: {
     type: {
       type: String,
---
+++ routes/observation.routes.js
@@ -0,0 +1,103 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const Observation = require("../models/Observation.model");
+
+router.get("/observations", (req, res, next) => {
+  Observation.find()
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      console.log("Error getting observations...", err);
+      res.status(500).json({ message: "Error getting observations..." });
+    });
+});
+
+router.post("/observations", (req, res, next) => {
+  const {
+    name,
+    date,
+    location,
+    habitat,
+    vegetation,
+    age,
+    photo,
+    sound,
+    temperature,
+    birdId,
+  } = req.body;
+
+  Observation.create({
+    name,
+    date,
+    location,
+    habitat,
+    vegetation,
+    age,
+    photo,
+    sound,
+    temperature,
+    birdId,
+  })
+    .then((response) => res.status(201).json(response))
+    .catch((err) => {
+      console.log("Error creating Observation");
+      res.status(500).json({ message: "Error creating project..." });
+    });
+});
+
+router.put("/observations/:observationId", (req, res, next) => {
+  const { observationId } = req.params;
+  const {
+    name,
+    date,
+    location,
+    habitat,
+    vegetation,
+    age,
+    photo,
+    sound,
+    temperature,
+    birdId,
+  } = req.body;
+
+  Observation.findByIdAndUpdate(
+    observationId,
+    {
+      name,
+      date,
+      location,
+      habitat,
+      vegetation,
+      age,
+      photo,
+      sound,
+      temperature,
+      birdId,
+    },
+    { new: true }
+  )
+    .then((updatedObservation) => {
+      res.json(updatedObservation);
+    })
+    .catch((err) => {
+      console.log("Error updating an project");
+      res.status(500).json({ message: "Error updating observation..." });
+    });
+});
+
+router.delete("/observations/:observationId", (req, res, next) => {
+  const { observationId } = req.params;
+  Observation.findByIdAndDelete(observationId)
+    .then(() => {
+      res.json({
+        message: `Observation with ${observationId} has been successfully removed`,
+      });
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Error updating observations..." })
+    );
+});
+
+module.exports = router;
